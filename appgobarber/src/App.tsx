ecb5091bb5284fc98865c0202a904d69862c0b38
LariSantin <lari_santin@hotmail.com>
2020-06-22 23:19:26 -0300

    commit parcial  mobile

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,7 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text} from 'react-native';
-const App = () => {
-  return <Text>Alo</Text>;
-};
+import { Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
+
+const App = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor= '#312e38'  />
+    <View style={{flex:1, backgroundColor: '#312e38' }}>
+        <Routes />
+    </View>
+  </NavigationContainer>
+
+);
 
 export default App;
