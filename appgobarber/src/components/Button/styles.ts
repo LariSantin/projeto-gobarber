4b45fad0e634a38c59a8dc42d6690dd718f21b49
LariSantin <lari_santin@hotmail.com>
2020-07-03 21:44:30 -0300

    finalizando telas de signIn e signUp

--- appgobarber/src/components/Button/styles.ts
+++ appgobarber/src/components/Button/styles.ts
@@ -6,12 +6,13 @@ export const Container = styled(RectButton)`
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
+  margin-top: 8px;
 
   justify-content: center;
   align-items: center;
 `;
 
-export const ButtonText = styled.View`
+export const ButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   color: #312e38;
   font-size: 18px;
