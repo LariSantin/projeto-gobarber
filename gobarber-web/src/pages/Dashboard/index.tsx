671e45557c74ffd5fb9400a2aca260c38e533e81
LariSantin <lari_santin@hotmail.com>
2021-01-25 23:28:40 -0300

    criando listagem

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import logo from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () =>  {
@@ -32,6 +41,35 @@ const Dashboard: React.FC = () =>  {
       </HeaderContent>
 
     </Header>
+
+    <Content>
+      <Schedule>
+        <h1>Horários agendados</h1>
+        <p>
+          <span>Hoje</span>
+          <span>Dia 6</span>
+          <span>Segunda-feira</span>
+        </p>
+
+        <NextAppointment>
+          <strong>Atendimento a seguir</strong>
+          <div>
+            <img
+              src="https://avatars.githubusercontent.com/u/23425156?s=460&u=63624331eb568edabe02c3f12b010b1edb1bb66b&v=4"
+              alt=""
+            />
+
+            <strong>LArissa santin</strong>
+
+            <span>
+              <FiClock />
+              0932
+            </span>
+          </div>
+        </NextAppointment>
+      </Schedule>
+      <Calendar />
+    </Content>
   </Container>
   );
 }
